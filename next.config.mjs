17f24df126e966333de08a8d9fb4cad2710555c5
Anurag Kundu <154730001+kunduanurag064@users.noreply.github.com>
2024-10-18 09:29:36 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        disableUserAgentParsing: true,  // Disable built-in user-agent parsing
+    },
+};
 
 export default nextConfig;
